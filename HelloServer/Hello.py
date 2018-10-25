7e2d93c5796b229b342d1831e25ef15f1fea13be
muskan09 <muskankalsi9@gmail.com>
2018-10-26 00:26:13 +0530

    headers

--- HelloServer/Hello.py
+++ HelloServer/Hello.py
@@ -3,3 +3,5 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 class HELLO(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
+        self.send_header('Content-type', 'text/plain; charset=utf-8')
+        self.end_headers()
