4329b5f81ca528d324a0c7d7405c4d7f96dfbba2
muskan09 <muskankalsi9@gmail.com>
2018-10-26 00:28:40 +0530

    main added

--- HelloServer/Hello.py
+++ HelloServer/Hello.py
@@ -5,4 +5,10 @@ class HELLO(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain; charset=utf-8')
         self.end_headers()
-        self.wfile.write("Hello, HTTP!\n".encode())
+        self.wfile.write("Hello\n".encode())
+
+if __name__ == '__main__':
+    server_address = ('',8000)  
+    httpd = HTTPServer(server_address, HELLO)
+    httpd.serve_forever()
+        
